76625dc3c324397dbac1e82b678b2f24006fdd61
Sergey Suslov <sergey.suslov.nsk@gmail.com>
2020-08-22 15:10:22 +0700

    tests: add publisher success test

--- pkg/tracing_test.go
+++ pkg/tracing_test.go
@@ -5,12 +5,55 @@ import (
 	"context"
 	"encoding/json"
 	kitnats "github.com/go-kit/kit/transport/nats"
+	"github.com/nats-io/nats-server/v2/server"
 	"github.com/nats-io/nats.go"
 	"github.com/openzipkin/zipkin-go"
 	"github.com/openzipkin/zipkin-go/model"
 	"github.com/openzipkin/zipkin-go/propagation/b3"
 	"testing"
+	"time"
 )
+
+var natsServer *server.Server
+
+func init() {
+	natsServer, _ = server.NewServer(&server.Options{
+		Host: "localhost",
+		Port: 4222,
+	})
+
+	go func() {
+		natsServer.Start()
+	}()
+
+	if ok := natsServer.ReadyForConnections(2 * time.Second); !ok {
+		panic("Failed start of NATS")
+	}
+}
+
+func newNatsConn(t *testing.T) *nats.Conn {
+	// Subscriptions and connections are closed asynchronously, so it's possible
+	// that there's still a subscription from an old connection that must be closed
+	// before the current test can be run.
+	for tries := 20; tries > 0; tries-- {
+		if natsServer.NumSubscriptions() == 0 {
+			break
+		}
+
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	if n := natsServer.NumSubscriptions(); n > 0 {
+		t.Fatalf("found %d active subscriptions on the server", n)
+	}
+
+	nc, err := nats.Connect("nats://"+natsServer.Addr().String(), nats.Name(t.Name()))
+	if err != nil {
+		t.Fatalf("failed to connect to gnatsd server: %s", err)
+	}
+
+	return nc
+}
 
 func TestNATSSubscriberTraceSuccess(t *testing.T) {
 	var (
@@ -66,9 +109,60 @@ func TestNATSSubscriberTraceOptions(t *testing.T) {
 		t.Fatalf("tags number differs, must be %d, found %d", len(testTags), len(spans[0].Tags))
 	}
 
-	for k, v := range spans[0].Tags {
-		if testTags[k] != v {
-			t.Fatalf("tags differs, must be %s, but found %s", testTags[k], v)
+	compareTags(t, testTags, spans[0].Tags)
+}
+
+func TestNATSPublisherTraceSuccess(t *testing.T) {
+	var (
+		testSubject = "nats.test"
+		testTags    = map[string]string{"tag1": "value1", "tag2": "value2"}
+	)
+	tr, rec := createTracer()
+	defer rec.Close()
+
+	nc := newNatsConn(t)
+
+	testMessage, _ := json.Marshal(testMessageData)
+	sub, err := nc.QueueSubscribe(testSubject, "natstracing", func(msg *nats.Msg) {
+		if err := nc.Publish(msg.Reply, testMessage); err != nil {
+			t.Fatal(err)
+		}
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer sub.Unsubscribe()
+
+	publisher := kitnats.NewPublisher(nc, testSubject, func(ctx context.Context, msg *nats.Msg, i interface{}) error {
+		msg.Data = i.([]byte)
+		return nil
+	}, func(ctx context.Context, msg *nats.Msg) (response interface{}, err error) {
+		return msg.Data, nil
+	}, NATSPublisherTrace(tr, Tags(testTags)))
+
+	res, _ := publisher.Endpoint()(context.Background(), testMessage)
+	response, ok := res.([]byte)
+	if !ok {
+		t.Fatal("response should be []byte")
+	}
+	if bytes.Compare(response, testMessage) != 0 {
+		t.Fatal("response message should be equal to request message")
+	}
+
+	spans := rec.Flush()
+	if len(spans) != 1 {
+		t.Fatalf("there must be only one span, found %d", len(spans))
+	}
+	if spans[0].Kind != model.Client {
+		t.Fatalf("the span must have Kind set to Client, found %s", spans[0].Kind)
+	}
+	compareTags(t, testTags, spans[0].Tags)
+}
+
+func compareTags(t *testing.T, tags1, tags2 map[string]string) {
+	for k, v := range tags1 {
+		if tags2[k] != v {
+			t.Fatalf("tags differs, must be %s, but found %s", v, tags2[k])
 		}
 	}
 }
