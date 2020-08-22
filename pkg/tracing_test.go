bad284ea1961e3aa64e0b2b16871280fe107d530
Sergey Suslov <sergey.suslov.nsk@gmail.com>
2020-08-22 11:40:34 +0700

    tests: add tags option test

--- pkg/tracing_test.go
+++ pkg/tracing_test.go
@@ -7,6 +7,7 @@ import (
 	kitnats "github.com/go-kit/kit/transport/nats"
 	"github.com/nats-io/nats.go"
 	"github.com/openzipkin/zipkin-go"
+	"github.com/openzipkin/zipkin-go/model"
 	"github.com/openzipkin/zipkin-go/propagation/b3"
 	"testing"
 )
@@ -15,41 +16,12 @@ func TestNATSSubscriberTraceSuccess(t *testing.T) {
 	var (
 		spanName = "test-span-name"
 	)
-	span, rec, tr := createSpanAndRecorder()
+	spans, testMessage, res := makeFakeNATSRequest(Name(spanName))
 
-	defer rec.Close()
-	testMessageWithContext, testMessage := createTestNatsMessageData(span)
-
-	response := make(chan []byte, 1)
-
-	handler := kitnats.NewSubscriber(
-		func(ctx context.Context, request interface{}) (response interface{}, err error) {
-			return request, nil
-		},
-		func(ctx context.Context, msg *nats.Msg) (interface{}, error) {
-			return msg.Data, nil
-		},
-		func(ctx context.Context, s string, conn *nats.Conn, i interface{}) error {
-			response <- i.([]byte)
-			return nil
-		},
-		NATSSubscriberTrace(tr, Name(spanName)),
-	)
-
-	var nc *nats.Conn
-	msg := &nats.Msg{
-		Data:  testMessageWithContext,
-		Reply: "anywhere",
-	}
-	handler.ServeMsg(nc)(msg)
-
-	res := <-response
-	span.Finish()
 	if bytes.Compare(testMessage, res) != 0 {
 		t.Fatal("Data was corrupted during the request")
 	}
 
-	spans := rec.Flush()
 	if len(spans) != 2 {
 		t.Fatalf("spans len equals to %d, t be 2", len(spans))
 	}
@@ -61,6 +33,66 @@ func TestNATSSubscriberTraceSuccess(t *testing.T) {
 	if *spans[0].ParentID != spans[1].ID {
 		t.Fatalf("Child span must have ParentId equal %s, but found %s", spans[1].ID, *spans[0].ParentID)
 	}
+}
+
+func TestNATSSubscriberTraceOptions(t *testing.T) {
+	var (
+		spanName = "test-span-name"
+		testTags = map[string]string{"tag1": "value1", "tag2": "value2"}
+	)
+
+	spans, _, _ := makeFakeNATSRequest(Name(spanName), Tags(testTags))
+
+	if len(spans[0].Tags) != len(testTags) {
+		t.Fatalf("tags number differs, must be %d, found %d", len(testTags), len(spans[0].Tags))
+	}
+
+	for k, v := range spans[0].Tags {
+		if testTags[k] != v {
+			t.Fatalf("tags differs, must be %s, but found %s", testTags[k], v)
+		}
+	}
+}
+
+func makeFakeNATSRequest(opts ...TracerOption) ([]model.SpanModel, []byte, []byte) {
+	span, rec, tr := createSpanAndRecorder()
+
+	defer rec.Close()
+
+	response := make(chan []byte, 1)
+
+	handler := createTransparentHandler(NATSSubscriberTrace(tr, opts...), response)
+
+	testMessageWithContext, testMessage := createTestNatsMessageData(span)
+	msg := &nats.Msg{
+		Data:  testMessageWithContext,
+		Reply: "anywhere",
+	}
+
+	var nc *nats.Conn
+	handler.ServeMsg(nc)(msg)
+
+	res := <-response
+	span.Finish()
+
+	spans := rec.Flush()
+	return spans, testMessage, res
+}
+
+func createTransparentHandler(o kitnats.SubscriberOption, response chan<- []byte) *kitnats.Subscriber {
+	return kitnats.NewSubscriber(
+		func(ctx context.Context, request interface{}) (response interface{}, err error) {
+			return request, nil
+		},
+		func(ctx context.Context, msg *nats.Msg) (interface{}, error) {
+			return msg.Data, nil
+		},
+		func(ctx context.Context, s string, conn *nats.Conn, i interface{}) error {
+			response <- i.([]byte)
+			return nil
+		},
+		o,
+	)
 }
 
 func createTestNatsMessageData(span zipkin.Span) (msgWithContext []byte, originalMsg []byte) {
